d03e5b2e9f26719786eac8d29dc5d49c8651feff
Mohammed <zeheerzak@gmail.com>
2023-12-17 22:31:13 +0530

    updated search page

--- frontend/src/components/TiniWiki.jsx
+++ frontend/src/components/TiniWiki.jsx
@@ -35,67 +35,134 @@ function TiniWiki() {
         searchResultsContainer.style.height = `${wikiContentHeight}px`;
       }
     };
-
     updateSearchResultsHeight();
   }, [selectedPageContent]);
 
+
   return (
     <div className="App bg-gray-100 min-h-screen font-sans">
-       <header className="bg-white border-b p-4">
-         <h1 className="text-2xl font-bold">TinyWiki</h1>
-         <div className="mt-2 flex">
-           <input
-            type="text"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            // className="border border-gray-300 px-2 py-1 rounded-l w-full"
-            className="border border-gray-300 px-2 py-1 rounded-l w-full focus:border-customBlue1 focus:outline-none"
+    <header className="bg-white border-b p-4">
+      <h1 className="text-2xl font-bold">TinyWiki</h1>
+      <div className="mt-2 flex">
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="border border-gray-300 px-2 py-1 rounded-l w-full focus:border-customBlue1 focus:outline-none"
+        />
+        <button
+          onClick={handleSearch}
+          className="bg-customBlue1 hover:bg-blue-600 text-white px-4 py-1 rounded-r ml-1"
+        >
+          <FaSearch />
+        </button>
+      </div>
+    </header>
+    <main className="flex flex-col md:flex-row">
+      <div className="w-full md:w-1/2 p-4">
+        <h2 className="text-xl font-semibold mt-4">Article</h2>
+        <div className="mt-2" id="searchResultsContainer">
+          {searchResults.length !== 0 ? (
+            <ul className="mt-2">
+              {searchResults.map((result) => (
+                <li
+                  key={result.pageid}
+                  onClick={() => handlePageClick(result.title)}
+                  className="cursor-pointer border-b py-2"
+                >
+                  <h3 className="text-blue-500 hover:underline">{result.title}</h3>
+                  <p
+                    className="text-gray-600"
+                    dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(result.snippet) }}
+                  ></p>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <img
+              src="https://www.wikipedia.org/portal/wikipedia.org/assets/img/Wikipedia-logo-v2@1.5x.png"
+              alt="tinywiki"
+              className="m-10"
+            />
+          )}
+        </div>
+      </div>
 
-          />
-          <button
-            onClick={handleSearch}
-            className="bg-customBlue1 hover:bg-blue-600 text-white px-4 py-1 rounded-r ml-1"
-          >
-            {/* Search */}
-            <FaSearch />
-          </button>
+      <div className="w-full md:w-1/2 p-4">
+        <h2 className="text-xl font-semibold mt-4">Content</h2>
+        <div className="mt-2 text-justify wiki-page-content" style={{ maxHeight: '900px', overflowY: 'auto' }}>
+          {selectedPageContent ? (
+            <div dangerouslySetInnerHTML={{ __html: selectedPageContent }} />
+          ) : (
+            <p>Select an article to view its content.</p>
+          )}
         </div>
-      </header>
-      <main className="flex flex-col md:flex-row">
-        <div className="w-full md:w-1/2 p-4">
-          <h2 className="text-xl font-semibold mt-4">Search Results</h2>
-          <div className="mt-2" id="searchResultsContainer">
-                    <ul className="mt-2">
-            {searchResults.map((result) => (
-              <li
-                key={result.pageid}
-                onClick={() => handlePageClick(result.title)}
-                className="cursor-pointer border-b py-2"
-              >
-                <h3 className="text-blue-500 hover:underline">{result.title}</h3>
-                <p
-                  className="text-gray-600"
-                  dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(result.snippet) }}
-                ></p>
-              </li>
-            ))}
-          </ul>
-          </div>
-        </div>
+      </div>
+    </main>
+  </div>
+);
 
-        <div className="w-full md:w-1/2 p-4">
-          <h2 className="text-xl font-semibold mt-4">Wiki Page Content</h2>
-          <div className="mt-2 text-justify wiki-page-content" style={{ maxHeight: '900px', overflowY: 'auto' }}>
-            {selectedPageContent ? (
-              <div dangerouslySetInnerHTML={{ __html: selectedPageContent }} />
-            ) : (
-              <p>Select an article to view its content.</p>
-            )}
-          </div>
-        </div>
-      </main>
-    </div>
-  );
 }
 
 export default TiniWiki;
+
+
+
+
+  // return (
+    // <div className="App bg-gray-100 min-h-screen font-sans">
+    //    <header className="bg-white border-b p-4">
+    //      <h1 className="text-2xl font-bold">TinyWiki</h1>
+    //      <div className="mt-2 flex">
+    //        <input
+    //         type="text"
+    //         value={searchTerm}
+    //         onChange={(e) => setSearchTerm(e.target.value)}
+    //         // className="border border-gray-300 px-2 py-1 rounded-l w-full"
+    //         className="border border-gray-300 px-2 py-1 rounded-l w-full focus:border-customBlue1 focus:outline-none"
+
+    //       />
+    //       <button
+    //         onClick={handleSearch}
+    //         className="bg-customBlue1 hover:bg-blue-600 text-white px-4 py-1 rounded-r ml-1"
+    //       >
+    //         {/* Search */}
+    //         <FaSearch />
+    //       </button>
+    //     </div>
+    //   </header>
+    //   <main className="flex flex-col md:flex-row">
+    //     <div className="w-full md:w-1/2 p-4">
+    //       <h2 className="text-xl font-semibold mt-4">Article</h2>
+    //       <div className="mt-2" id="searchResultsContainer">
+    //                 <ul className="mt-2">
+    //         {searchResults.map((result) => (
+    //           <li
+    //             key={result.pageid}
+    //             onClick={() => handlePageClick(result.title)}
+    //             className="cursor-pointer border-b py-2"
+    //           >
+    //             <h3 className="text-blue-500 hover:underline">{result.title}</h3>
+    //             <p
+    //               className="text-gray-600"
+    //               dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(result.snippet) }}
+    //             ></p>
+    //           </li>
+    //         ))}
+    //       </ul>
+    //       </div>
+    //     </div>
+
+    //     <div className="w-full md:w-1/2 p-4">
+    //       <h2 className="text-xl font-semibold mt-4">Content</h2>
+    //       <div className="mt-2 text-justify wiki-page-content" style={{ maxHeight: '900px', overflowY: 'auto' }}>
+    //         {selectedPageContent ? (
+    //           <div dangerouslySetInnerHTML={{ __html: selectedPageContent }} />
+    //         ) : (
+    //           <p>Select an article to view its content.</p>
+    //         )}
+    //       </div>
+    //     </div>
+    //   </main>
+    // </div> 
+  // );
